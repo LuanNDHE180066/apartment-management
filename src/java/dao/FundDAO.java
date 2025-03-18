8fe9eca53813ac0eeffecf39502b838665a7d69a
thanhhui1709 <thanhhui1709@gmail.com>
2025-03-18 09:34:16 +0700

    view fund

--- src/java/dao/FundDAO.java
+++ src/java/dao/FundDAO.java
@@ -7,6 +7,7 @@ package dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import jdbc.DBContext;
 import model.Fund;
@@ -18,21 +19,39 @@ import model.InvoiceDetail;
  *
  * @author PC
  */
-public class FundDAO extends DBContext{
-    public Fund getById(String id){
-        String sql = "select * from Fund where id ="+id;
-        try{
+public class FundDAO extends DBContext {
+
+    public Fund getById(String id) {
+        String sql = "select * from Fund where id =" + id;
+        try {
             PreparedStatement st = connection.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
-            while(rs.next()){
-                return new Fund(rs.getInt("id"),rs.getString("name"),rs.getString("description"),rs.getString("startdate"),rs.getFloat("value"));
+            while (rs.next()) {
+                return new Fund(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getString("startdate"), rs.getFloat("value"));
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e);
         }
         return null;
     }
-    public void revenueFundByInvoice(String id){
+
+    public List<Fund> getAll() {
+        String sql = "select * from Fund";
+        List<Fund> list = new ArrayList<>();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                list.add(new Fund(rs.getInt("id"), rs.getString("name"), rs.getString("description"), rs.getString("startdate"), rs.getFloat("value")));
+            }
+            return list;
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public void revenueFundByInvoice(String id) {
         InvoiceDAO ivd = new InvoiceDAO();
         InvoiceDetalDAO ivdd = new InvoiceDetalDAO();
         FundHistoryDAO fhd = new FundHistoryDAO();
@@ -42,16 +61,17 @@ public class FundDAO extends DBContext{
         fhd.insertFundHistory(ivds, iv.getResident(), 1);
         String sql = "update Fund set value = ?, ChangeDate=? where id = 1";
         try {
-            PreparedStatement st =connection.prepareStatement(sql);
-            st.setFloat(1, f.getValue()+iv.getTotal());
-            st.setDate(2,new java.sql.Date(System.currentTimeMillis()));
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setFloat(1, f.getValue() + iv.getTotal());
+            st.setDate(2, new java.sql.Date(System.currentTimeMillis()));
             st.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e);
         }
     }
+
     public static void main(String[] args) {
         FundDAO dao = new FundDAO();
-        System.out.println(""+dao.getById("1"));
+        System.out.println("" + dao.getById("1").getName());
     }
 }
--- src/java/dao/FundHistoryDAO.java
+++ src/java/dao/FundHistoryDAO.java
@@ -8,6 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import jdbc.DBContext;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.FundHistory;
 import model.InvoiceDetail;
 import model.Resident;
 
@@ -15,19 +18,52 @@ import model.Resident;
  *
  * @author PC
  */
-public class FundHistoryDAO extends DBContext{
-    public void insertFundHistory(List<InvoiceDetail> ivds, Resident rs, int fundid){
+public class FundHistoryDAO extends DBContext {
+
+    public void insertFundHistory(List<InvoiceDetail> ivds, Resident rs, int fundid) {
         for (InvoiceDetail ivd : ivds) {
-            String sql = "insert into FundHistory(value_befor,value_after,name,date,fundid)\n" +
-            "  values(?,?,?,?,?)";
+            String sql = "insert into FundHistory(value_befor,value_after,name,date,fundid)\n"
+                    + "  values(?,?,?,?,?)";
             try {
-            PreparedStatement st =connection.prepareStatement(sql);
-            st.setFloat(1, ivd.getAmount());
-            st.setDate(4,new java.sql.Date(System.currentTimeMillis()));
-            st.executeUpdate();
+                PreparedStatement st = connection.prepareStatement(sql);
+                st.setFloat(1, ivd.getAmount());
+                st.setDate(4, new java.sql.Date(System.currentTimeMillis()));
+                st.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public List<FundHistory> getByFundID(int id) {
+        String sql = "select * from FundHistory where fundid=? ";
+        FundDAO fd = new FundDAO();
+        List<FundHistory> list = new ArrayList<>();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                FundHistory fh = new FundHistory();
+                fh.setId(rs.getInt(1));
+                fh.setValue_befor(rs.getFloat(2));
+                fh.setValue_after(rs.getFloat(3));
+                fh.setDate(rs.getString(4));
+                fh.setName(rs.getString(5));
+                fh.setFundid(fd.getById(rs.getString(6)));
+                list.add(fh);
+
+            }
+            return list;
         } catch (SQLException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        FundHistoryDAO fd = new FundHistoryDAO();
+        System.out.println(fd.getByFundID(1));
     }
 }
