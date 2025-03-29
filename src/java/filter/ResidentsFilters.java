1f9a65c2629500190d398431881da1bdd26d8e1b
quangpnhe180573 <quangdpcl@gmail.com>
2025-03-29 19:25:02 +0700

    fix ui, filter

--- src/java/filter/ResidentsFilters.java
+++ src/java/filter/ResidentsFilters.java
@@ -25,11 +25,11 @@ import model.Account;
  *
  * @author quang
  */
-@WebFilter(filterName = "ResidentsFilters", urlPatterns = {"/registerNewLivingOrOwnerResident.jsp, "
-    + "/register-new-living-or-owner-resident",
+@WebFilter(filterName = "ResidentsFilters", urlPatterns = {
+    "/registerNewLivingOrOwnerResident.jsp",
+    "/register-new-living-or-owner-resident",
     "/changeRepresentResident.jsp",
     "/change-represent-resident"
-
 })
 public class ResidentsFilters implements Filter {
 
@@ -126,7 +126,7 @@ public class ResidentsFilters implements Filter {
             return;
         }
 
-        if (a.getRoleId() != 1 || a.getRoleId() != 6) {
+        if (a.getRoleId() != 1) {
             res.sendRedirect("404_error.jsp");
         }
 
